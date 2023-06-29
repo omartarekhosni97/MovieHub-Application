cbe3ea253228eb97beca176012ae1a3920891b69
omar tarek <omartarekhosni309@gmail.com>
2023-06-30 01:03:09 +0300

    shoppingcart-icon fix

--- Data/Cart/ShoppingCart.cs
+++ Data/Cart/ShoppingCart.cs
@@ -73,6 +73,7 @@ namespace MovieHub.Data.Cart
             var items = await _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).ToListAsync();
             _context.ShoppingCartItems.RemoveRange(items);
             await _context.SaveChangesAsync();
+            ShoppingCartItems = new List<ShoppingCartItem>();
         }
 
     }
